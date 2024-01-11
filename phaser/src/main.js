f7f4119eb0f6433cf829caa285ca18f2e2b0112a
sophia-bouck <sophia.bouck@gmail.com>
2024-01-10 16:13:35 -0800

    added card dealing animation, issue with image loading

---
+++ phaser/src/main.js
@@ -0,0 +1,33 @@
+import Phaser from "phaser";
+import TitleScreen from "./scenes/TitleScreen";
+import Game from "./scenes/Game";
+import Background from "./scenes/Background";
+import WinScreen from "./scenes/WinScreen";
+import LossScreen from "./scenes/LossScreen";
+import Animation_Testing from "./scenes/Animation-Testing";
+
+const config = {
+  width: 800,
+  height: 500,
+  type: Phaser.AUTO, // this means phaser handles if webgl or canvas is used
+  backgroundColor: "#7f7f7f",
+  physics: {
+    // this is set for pong purposes..
+    // default: "arcade",
+    // arcade: {
+    //   gravity: { y: 0 },
+    //   debug: true,
+    // },
+  },
+};
+
+const game = new Phaser.Game(config);
+
+//game.scene.add("titlescreen", TitleScreen);
+// game.scene.add("game", Game);
+// game.scene.add("background", Background);
+// game.scene.add("win", WinScreen);
+// game.scene.add("loss", LossScreen);
+game.scene.add("animation testing", Animation_Testing);
+
+game.scene.start("animation testing");
---
+++ phaser/src/scenes/TitleScreen.js
@@ -0,0 +1,20 @@
+import Phaser from "phaser";
+
+export default class TitleScreen extends Phaser.Scene {
+  preload() {}
+  create() {
+    // const title = this.add.text(400, 250, "Phaser Test - Pong", {
+    //   fontSize: 50,
+    //   fontFamily: "arial",
+    // });
+    // title.setOrigin(0.5, 0.5);
+    // const subtitle = this.add.text(400, 350, "Press Space to Start", {
+    //   fontSize: 25,
+    //   fontFamily: "arial",
+    // });
+    // subtitle.setOrigin(0.5, 0.5);
+    // this.input.keyboard.once("keydown-SPACE", () => {
+    //   this.scene.start("game");
+    // });
+  }
+}
