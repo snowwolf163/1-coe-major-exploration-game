783b315c3067706c0e2f29c71474935f8ea233d2
sophia-bouck <sophia.bouck@gmail.com>
2024-04-26 16:13:01 -0700

    all course cards added to decks

--- client/src/helpers/DeckHandler.js
+++ client/src/helpers/DeckHandler.js
@@ -1,5 +1,35 @@
 import CardBack from "./cards/CardBack";
 import CourseCard from "./cards/CourseCard";
+import {
+  CourseCS46X,
+  CourseCS261,
+  CourseCS290,
+  CourseCS344,
+  CourseCS361,
+  CourseCS362,
+  CourseCS381,
+  CourseCS391,
+  CourseCS444,
+  CourseCS475,
+  CourseCHE444,
+  CourseCHE499,
+  CourseECE271,
+  CourseECE322,
+  CourseECE341,
+  CourseECE351,
+  CourseECE391,
+  CourseENGR201,
+  CourseENGR202,
+  CourseENGR203,
+  CoursePH211,
+  CoursePH212,
+  CoursePH481,
+  CourseCS161,
+  CourseCS162,
+  CourseENGR100,
+  CourseENGR102,
+  CourseENGR103,
+} from "./cards/CourseCard";
 import DebtCard from "./cards/DebtCard";
 import { MajorCard } from "./cards/MajorCard";
 import {
@@ -28,6 +58,36 @@ export default class DeckHandler {
         skipCard: new SkipCard(scene),
         // course cards
         courseCard: new CourseCard(scene), //placeholder
+        // course cards
+        courseCS46X: new CourseCS46X(scene),
+        courseCS261: new CourseCS261(scene),
+        courseCS290: new CourseCS290(scene),
+        courseCS344: new CourseCS344(scene),
+        courseCS361: new CourseCS361(scene),
+        courseCS362: new CourseCS362(scene),
+        courseCS381: new CourseCS381(scene),
+        courseCS391: new CourseCS391(scene),
+        courseCS444: new CourseCS444(scene),
+        courseCS475: new CourseCS475(scene),
+        courseCHE444: new CourseCHE444(scene),
+        courseCHE499: new CourseCHE499(scene),
+        courseECE271: new CourseECE271(scene),
+        courseECE322: new CourseECE322(scene),
+        courseECE341: new CourseECE341(scene),
+        courseECE351: new CourseECE351(scene),
+        courseECE391: new CourseECE391(scene),
+        courseENGR201: new CourseENGR201(scene),
+        courseENGR202: new CourseENGR202(scene),
+        courseENGR203: new CourseENGR203(scene),
+        coursePH211: new CoursePH211(scene),
+        coursePH212: new CoursePH212(scene),
+        coursePH481: new CoursePH481(scene),
+        courseCS161: new CourseCS161(scene),
+        courseCS162: new CourseCS162(scene),
+        courseENGR100: new CourseENGR100(scene),
+        courseENGR102: new CourseENGR102(scene),
+        courseENGR103: new CourseENGR103(scene),
+
         // major cards
         majorCard: new MajorCard(scene), //placeholder
         majorBE: new MajorBE(scene),
--- client/src/helpers/cards/CourseCard.js
+++ client/src/helpers/cards/CourseCard.js
@@ -9,11 +9,260 @@ export default class CourseCard extends Card {
   }
 }
 
-// export class CourseCS46X extends Card {
-//   constructor(scene) {
-//     super(scene);
-//     this.name = "courseCard";
-//     this.playerCardSprite = "course-card";
-//     this.opponentCardSprite = "course-card";
-//   }
-// }
+// CS courses
+
+export class CourseCS46X extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS46X";
+    this.playerCardSprite = "course-CS46X";
+    this.opponentCardSprite = "course-CS46X";
+  }
+}
+
+export class CourseCS261 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS261";
+    this.playerCardSprite = "course-CS261";
+    this.opponentCardSprite = "course-CS261";
+  }
+}
+
+export class CourseCS290 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS290";
+    this.playerCardSprite = "course-CS290";
+    this.opponentCardSprite = "course-CS290";
+  }
+}
+
+export class CourseCS344 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS344";
+    this.playerCardSprite = "course-CS344";
+    this.opponentCardSprite = "course-CS344";
+  }
+}
+
+export class CourseCS361 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS361";
+    this.playerCardSprite = "course-CS361";
+    this.opponentCardSprite = "course-CS361";
+  }
+}
+
+export class CourseCS362 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS362";
+    this.playerCardSprite = "course-CS362";
+    this.opponentCardSprite = "course-CS362";
+  }
+}
+
+export class CourseCS381 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS381";
+    this.playerCardSprite = "course-CS381";
+    this.opponentCardSprite = "course-CS381";
+  }
+}
+
+export class CourseCS391 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS391";
+    this.playerCardSprite = "course-CS391";
+    this.opponentCardSprite = "course-CS391";
+  }
+}
+
+export class CourseCS444 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS444";
+    this.playerCardSprite = "course-CS444";
+    this.opponentCardSprite = "course-CS444";
+  }
+}
+
+export class CourseCS475 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS475";
+    this.playerCardSprite = "course-CS475";
+    this.opponentCardSprite = "course-CS475";
+  }
+}
+
+// ECE courses
+
+export class CourseCHE444 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCHE444";
+    this.playerCardSprite = "course-CHE444";
+    this.opponentCardSprite = "course-CHE444";
+  }
+}
+
+export class CourseCHE499 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCHE499";
+    this.playerCardSprite = "course-CHE499";
+    this.opponentCardSprite = "course-CHE499";
+  }
+}
+
+export class CourseECE271 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseECE271";
+    this.playerCardSprite = "course-ECE271";
+    this.opponentCardSprite = "course-ECE271";
+  }
+}
+
+export class CourseECE322 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseECE322";
+    this.playerCardSprite = "course-ECE322";
+    this.opponentCardSprite = "course-ECE322";
+  }
+}
+
+export class CourseECE341 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseECE341";
+    this.playerCardSprite = "course-ECE341";
+    this.opponentCardSprite = "course-ECE341";
+  }
+}
+
+export class CourseECE351 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseECE351";
+    this.playerCardSprite = "course-ECE351";
+    this.opponentCardSprite = "course-ECE351";
+  }
+}
+
+export class CourseECE391 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseECE391";
+    this.playerCardSprite = "course-ECE391";
+    this.opponentCardSprite = "course-ECE391";
+  }
+}
+
+export class CourseENGR201 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseENGR201";
+    this.playerCardSprite = "course-ENGR201";
+    this.opponentCardSprite = "course-ENGR201";
+  }
+}
+
+export class CourseENGR202 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseENGR202";
+    this.playerCardSprite = "course-ENGR202";
+    this.opponentCardSprite = "course-ENGR202";
+  }
+}
+
+export class CourseENGR203 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseENGR203";
+    this.playerCardSprite = "course-ENGR203";
+    this.opponentCardSprite = "course-ENGR203";
+  }
+}
+
+export class CoursePH211 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "coursePH211";
+    this.playerCardSprite = "course-PH211";
+    this.opponentCardSprite = "course-PH211";
+  }
+}
+
+export class CoursePH212 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "coursePH212";
+    this.playerCardSprite = "course-PH212";
+    this.opponentCardSprite = "course-PH212";
+  }
+}
+
+export class CoursePH481 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "coursePH481";
+    this.playerCardSprite = "course-PH481";
+    this.opponentCardSprite = "course-PH481";
+  }
+}
+
+// "multi" courses
+
+export class CourseCS161 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS161";
+    this.playerCardSprite = "course-CS161";
+    this.opponentCardSprite = "course-CS161";
+  }
+}
+
+export class CourseCS162 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseCS162";
+    this.playerCardSprite = "course-CS162";
+    this.opponentCardSprite = "course-CS162";
+  }
+}
+
+export class CourseENGR100 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseENGR100";
+    this.playerCardSprite = "course-ENGR100";
+    this.opponentCardSprite = "course-ENGR100";
+  }
+}
+
+export class CourseENGR102 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseENGR102";
+    this.playerCardSprite = "course-ENGR102";
+    this.opponentCardSprite = "course-ENGR102";
+  }
+}
+
+export class CourseENGR103 extends Card {
+  constructor(scene) {
+    super(scene);
+    this.name = "courseENGR103";
+    this.playerCardSprite = "course-ENGR103";
+    this.opponentCardSprite = "course-ENGR103";
+  }
+}
